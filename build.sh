cdaf2f4b6166e4f6d6642682da101eeffc16a8c3
Márton Elek <elek@apache.org>
2022-04-02 17:22:09 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 TAG=$(git log -n 1 --pretty=format:"%h")
-docker build -t ghcr.io/elek/erigon:$TAG
+docker build -t ghcr.io/elek/erigon:$TAG .
 docker push ghcr.io/elek/erigon:$TAG
